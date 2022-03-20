8d286de60d214549ecba3c7f8b3d8ddd63945ca2
mixxxon <clipping.master@gmail.com>
2022-03-20 17:52:08 +0100

    Hamburger menu on small/medium displays created and styled

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 document.addEventListener("DOMContentLoaded", function(evt) {
 
-    
+    // Hamburger menu (Handle website menu on small/medium displays)
+    var hamburgerBtn = document.getElementsByClassName("hamburger")[0];
+    var sidenav = document.getElementsByClassName("w3-sidenav")[0];
+
+    hamburgerBtn.addEventListener("click", function(){
+        if(hamburgerBtn.textContent == "menu"){
+            hamburgerBtn.textContent = "close";
+            sidenav.style.display = "block";
+
+        } else {
+            hamburgerBtn.textContent = "menu";
+            sidenav.style.display = "none";
+        }
+    });
 
 });
